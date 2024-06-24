80d5e7dffbe242ab8d82d2c5d043155e67fa8baa
Michael Atolwa <atolwamichael7@gmail.com>
2024-06-24 09:54:16 +0300

    converted the intergers to string values and concatenated them for a more seamless code execution.

--- python_introduction/basic_operation.py
+++ python_introduction/basic_operation.py
@@ -4,12 +4,14 @@ number2 = 5
 
 # calculate the sum of number1 and number2
 sum = number1 + number2
-print("Addition of 10 and 5 is " + str(sum))
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum))
 
 # calculate the difference of number2 from number1
 diff = number1 - number2
-print("Subtraction of 10 and 5 is " + str(diff))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+
 
 # calculate the product of number1 by number2
 product = number1 * number2
-print("Multiplication of 10 and 5 is " + str(product))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+
