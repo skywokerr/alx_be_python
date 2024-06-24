a9e46117cf3298ccb312a537bfa067340b141770
Michael Atolwa <atolwamichael7@gmail.com>
2024-06-24 10:16:39 +0300

    createda simple calculator that calculates simple interest.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+# formula for simple interest is (I = P * R * T)
+#( I ) is the interest earned,
+#( P ) is the principal amount (initial investment),
+#( R ) is the annual interest rate (as a decimal),
+#( T ) is the time the money is invested for in years.
+
+principal = 1000 # representing $1000
+rate = 0.05 # representing 5% annual interest rate
+time = 3 # representing 3 years
+
+interest = principal * rate * time
+print("The simple interest is: " + str(interest))
