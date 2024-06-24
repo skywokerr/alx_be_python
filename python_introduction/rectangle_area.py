0051ef29aeacf1d6a9dfde425c9174353654d35d
Michael Atolwa <atolwamichael7@gmail.com>
2024-06-24 10:25:50 +0300

    created a .py script that prints the area of a rectangle.

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,7 @@
+# creating a script that calculates the area of a rectangle
+# formula (Area = length × width)
+length = 10
+width = 5
+
+area = length * width
+print("The area of the rectangle is: " + str(area))  # prints the area of the rectangle
