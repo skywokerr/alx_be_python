52a4c9595d6f9e82b3529116ea3a2900134a151b
Michael Atolwa <atolwamichael7@gmail.com>
2024-07-01 13:12:18 +0300

    updated the code flow of the mulptiplication program.

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,16 +1,9 @@
 # multiplication_table.py
 
-def get_number(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
-
 # Prompt the user to enter a number
-number = get_number("Enter a number to see its multiplication table: ")
+number = int(input("Enter a number to see its multiplication table: "))
 
 # Generate and print the multiplication table from 1 to 10
-for i in range(1, 10 + 1):
+for i in range(1, 11):
     product = number * i
     print(f"{number} * {i} = {product}")
